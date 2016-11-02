7e076a12323f31a64607d3bac60bb3fcba765462
zhuohf1 <zhuohf1@lenovo.com>
2016-11-02 16:04:11 +0800

    modify the html, css

--- js/plugin.js
+++ js/plugin.js
@@ -5,12 +5,12 @@
 	var Pic=function(){
 		$(".pic").mouseenter(function(){
 			//$(this).css("background","#000");
-		});	
+		});
 		$(".pic").mouseleave()(function(){
 			//$(this).css("background","#f1f1f1");
-		});	
+		});
 	}
-	
+
 	//regeister Plugin
 	//===========================
 	$(window).on('load',function(){
@@ -19,7 +19,7 @@
 	});
 	$(window).scroll(function(){
 		if($(window).scrollTop()>500){
-			$(".btt").show();	
+			$(".btt").show();
 		}
 		else{
 			$(".btt").hide();
